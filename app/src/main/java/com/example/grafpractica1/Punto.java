54de5080db0fdc31d9da2c2537dfbf0e04a74bdc
jxse <168609551+jxsewv@users.noreply.github.com>
2025-02-22 23:17:45 -0600

    Actualizacion punto.

---
+++ app/src/main/java/com/example/grafpractica1/Punto.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.grafpractica1;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+/**
+ *
+ * @author chuch
+ */
+public class Punto {
+    private float Px;
+    private float Py;
+    
+    
+    /**
+     * @return the Px
+     */
+    public float getPx() {
+        return Px;
+    }
+
+    /**
+     * @param Px the Px to set
+     */
+    public void setPx(float Px) {
+        this.Px = Px;
+    }
+
+    /**
+     * @return the Py
+     */
+    public float getPy() {
+        return Py;
+    }
+
+    /**
+     * @param Py the Py to set
+     */
+    public void setPy(float Py) {
+        this.Py = Py;
+    }
+
+    public Punto(float Px, float Py) {
+        this.Px = Px;
+        this.Py = Py;
+    }
+
+    public void Dibujar (ShapeRenderer shpRender)
+    {
+        shpRender.setColor(Color.FOREST);
+        shpRender.circle(getPx()*Figura.escala,getPy()*Figura.escala, 8);
+        
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getPx() + "," + getPy() + ")";
+    }
+    
+    
+}
