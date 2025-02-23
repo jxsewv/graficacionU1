07b1a705e19d299e8110062580219e7b736be327
jxse <168609551+jxsewv@users.noreply.github.com>
2025-02-22 23:17:28 -0600

    Actualizacion figura.

---
+++ app/src/main/java/com/example/grafpractica1/Figura.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.grafpractica1;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author chuch
+ */
+public class Figura {
+
+    public static int escala = 20;
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the puntos
+     */
+    public DefaultListModel<Punto> getListaPuntos() {
+        if(listaPuntos==null)
+        {
+            listaPuntos = new DefaultListModel<>();
+            
+        }
+        return listaPuntos;
+    }
+
+    /**
+     * @param puntos the puntos to set
+     */
+    public void setListaPuntos(DefaultListModel<Punto> puntos) {
+        this.listaPuntos = puntos;
+    }
+    private String nombre;
+    private DefaultListModel<Punto> listaPuntos;
+
+    public Figura(String nombre) {
+        this.nombre = nombre;
+        
+        //getListaPuntos().addElement(new Punto(50,50));
+        //getListaPuntos().addElement(new Punto(50,100));
+       
+    }
+    public void dibujar(ShapeRenderer shpRender){
+        for (int i = 0; i < getListaPuntos().size(); i++){
+            Punto p = getListaPuntos().getElementAt(i);
+            p.Dibujar(shpRender);
+        }
+    }
+    @Override
+    public String toString(){
+        return getNombre();
+    }
+    
+}
