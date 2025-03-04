1884f84807623b83d8b95c7859a65d12856be822
jxse <168609551+jxsewv@users.noreply.github.com>
2025-03-04 17:58:43 -0600

    actualizar eliminar en ventana principal.

--- app/src/main/java/com/example/grafpractica1/VentanaPrincipal.java
+++ app/src/main/java/com/example/grafpractica1/VentanaPrincipal.java
@@ -434,7 +434,11 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        if (figuraSeleccionada != null)
+        {
+            canvas.listaFiguras.removeElement(figuraSeleccionada);
+            jList1.updateUI();
+        }
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
