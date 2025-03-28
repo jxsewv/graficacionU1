07c9cbad9224250e2f24e4f7d91a26c90b01d386
jxse <168609551+jxsewv@users.noreply.github.com>
2025-03-27 22:21:39 -0600

    clase operaciones2d.

---
+++ app/src/main/java/com/example/grafpractica1/Operaciones2D.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.grafpractica1;
+
+/**
+ *
+ * @author chuch
+ */
+public class Operaciones2D {
+    public static Matriz33 getMatrizIdentidad()
+    {
+        return new Matriz33(1f, 0, 0, 0, 1f, 0, 0, 0, 1f);
+    }
+    
+}
