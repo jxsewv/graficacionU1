97b3ca1e1c235142a287a8aa5bb2cdea2110ae2e
jxse <168609551+jxsewv@users.noreply.github.com>
2025-03-27 23:03:11 -0600

    cambios matriz 31.

--- app/src/main/java/com/example/grafpractica1/matriz31.java
+++ app/src/main/java/com/example/grafpractica1/matriz31.java
@@ -9,5 +9,32 @@ package com.example.grafpractica1;
  * @author chuch
  */
 public class matriz31 {
+        public float [] datos;
+
+    public matriz31() {
+        datos = new float [3];
+    }
+    public matriz31(float n0,
+                    float n1,
+                    float n2
+  
+    ) {
+        datos = new float [3];
+        datos [0] = n0;
+        datos [1] = n1;
+        datos [2] = n2;
+
+    }    
+
+    @Override
+    public String toString() {
+        String s = "";
+
+            for (int j = 0; j < 3; j++) {
+                s += datos [j] + ",";
+            }
+
+        return s;
     
+}
 }
