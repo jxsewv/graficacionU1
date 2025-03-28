260f16d90506e561181a1181cb2e91d5c0438320
jxse <168609551+jxsewv@users.noreply.github.com>
2025-03-27 23:02:21 -0600

    cambios operaciones 2d

--- app/src/main/java/com/example/grafpractica1/Operaciones2D.java
+++ app/src/main/java/com/example/grafpractica1/Operaciones2D.java
@@ -14,4 +14,17 @@ public class Operaciones2D {
         return new Matriz33(1f, 0, 0, 0, 1f, 0, 0, 0, 1f);
     }
     
+    public static Matriz33 getMatrizTraslacion(float tx, float ty)
+    {
+        Matriz33 m = getMatrizIdentidad();
+        m.datos[2][0] = tx;
+        m.datos[2][1] = ty;
+        return m;
+        
+    }
+    public static matriz31 MultiplicarM33xM31(Matriz33 m33, matriz31 m31)
+    {
+        matriz31 resultado = new matriz31(0,0,1);
+        return resultado;
+    }
 }
